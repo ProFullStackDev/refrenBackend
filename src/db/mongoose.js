0197241e299d71c31eb8e11ea7dd214d23433c93
GodFather93 <suryapalsingh.inda@gmail.com>
2020-12-24 20:34:03 +0530

    server changes

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 // Db connection pool
 const connectionURL =
-  process.env.MONGODB_URL ||
   'mongodb+srv://Surya:surya007@cluster0-bwfkc.mongodb.net/Refren?retryWrites=true&w=majority';
 
 mongoose.connect(connectionURL, {
